a0e17a45b7e17a0a5c5a0054cb8a5f78d31e0116
lizu02 <livio.zurflueh@students.fhnw.ch>
2025-03-19 11:43:04 +0100

    Uebung2_Aufg1_hinzugefügt

---
+++ Uebung02/Uebung02_Aufg1.py
@@ -0,0 +1,23 @@
+
+
+
+class vektor3:
+    def __init__(self, x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+a = vektor3(3,4,2)
+b = vektor3(2,1,0)
+
+def __str__(self):
+    return f"x{self.x},y{self.y},z{self.z}"
+
+
+
+print(a)
+
+
+
+
